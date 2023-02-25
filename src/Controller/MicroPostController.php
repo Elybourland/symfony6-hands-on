90492abc53533e420a33035a46a6f52b650aafdc
Elybourland <JEBourland@yahoo.com>
2023-02-24 20:01:32 -0800

    doctrine repositories - fetching, storing, updating, deleting data

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +12,30 @@ use Symfony\Component\Routing\Annotation\Route;
 class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
-    public function index(): Response
+    public function index(MicroPostRepository $posts): Response
     {
+
+        // Adding AKA saving a post
+
+        // $microPost = new MicroPost();
+        // $microPost->setTitle('It comes from controller');
+        // $microPost->setText('Hi!');
+        // $microPost->setCreated(new DateTime());
+
+        // Updating a post
+
+        // $microPost = $posts->find(4);
+        // $microPost->setTitle('Welcome in general!');
+
+        // Removing a post
+
+        $microPost = $posts->find(4);
+        $posts->remove($microPost, true);
+
+        // Use below save method for adding and updating
+        // $posts->save($microPost, true);
+
+        // dd($posts->findBy(['title' => 'Welcome to US!']));
         return $this->render('micro_post/index.html.twig', [
             'controller_name' => 'MicroPostController',
         ]);
