0d0ed259221fcf0c7ef9185769c7c6f9816da76f
Elybourland <JEBourland@yahoo.com>
2023-02-24 20:21:53 -0800

    param converter

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -14,7 +14,6 @@ class MicroPostController extends AbstractController
     #[Route('/micro-post', name: 'app_micro_post')]
     public function index(MicroPostRepository $posts): Response
     {
-
         // Adding AKA saving a post
 
         // $microPost = new MicroPost();
@@ -29,15 +28,24 @@ class MicroPostController extends AbstractController
 
         // Removing a post
 
-        $microPost = $posts->find(4);
-        $posts->remove($microPost, true);
+        // $microPost = $posts->find(4);
+        // $posts->remove($microPost, true);
 
         // Use below save method for adding and updating
         // $posts->save($microPost, true);
 
         // dd($posts->findBy(['title' => 'Welcome to US!']));
+
+        dd($posts->findAll());
         return $this->render('micro_post/index.html.twig', [
             'controller_name' => 'MicroPostController',
         ]);
+    }
+
+
+    #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    public function showOne(MicroPost $post): Response
+    {
+        dd($post);
     }
 }
